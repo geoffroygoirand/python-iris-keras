2ff6bcbdff1b999dc830792ea64718c2801e6a89
geoffroygoirand <37084297+geoffroygoirand@users.noreply.github.com>
2022-03-23 11:08:11 +0100

    Update isis.py

--- isis.py
+++ isis.py
@@ -3,6 +3,9 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD,Adam
 
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
 
 model = Sequential()
 
@@ -10,6 +13,8 @@ model.add(Dense(15,input_shape=(4,),activation='tanh'))
 model.add(Dense(10,activation='tanh'))
 model.add(Dense(6,activation='tanh'))
 model.add(Dense(3,activation='softmax'))
+
+model.fit(X,Y, epochs=5)
 
 model.compile(Adam(lr=0.06),'categorical_crossentropy',metrics=['accuracy'])
 
